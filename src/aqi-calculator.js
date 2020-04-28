bbad327b4a93b0f2db022824d9c2fa1db4fb98f6
Restyled.io <commits@restyled.io>
2020-04-28 18:24:44 +0000

    Restyled by clang-format

--- src/aqi-calculator.js
+++ src/aqi-calculator.js
@@ -1,28 +1,29 @@
 const dayjs = require("dayjs");
-const { BANDS, HOUR_DIFF } = require("./utils");
+const {BANDS, HOUR_DIFF} = require("./utils");
 
 const calculateSubIndex = (arr = [], pollutant) => {
   // If not a known pollutant
-  if (!Object.keys(HOUR_DIFF).includes(pollutant)) return 0;
+  if (!Object.keys(HOUR_DIFF).includes(pollutant))
+    return 0;
 
   // FIlter the values based on hours and null check
   const now = dayjs();
   // Hour diff specific to pollutant
   const hourDiff = HOUR_DIFF[pollutant];
-  const filtered = arr.filter(
-    ({ datetime, value }) =>
-      value && now.diff(dayjs(datetime), "hours") < hourDiff
-  );
+  const filtered =
+      arr.filter(({datetime, value}) =>
+                     value && now.diff(dayjs(datetime), "hours") < hourDiff);
 
   // If no values left, aqi is considered 0
-  if (filtered.length <= 0) return 0;
+  if (filtered.length <= 0)
+    return 0;
 
   // Average out the values
   const mean =
-    filtered.reduce((acc, { value }) => acc + value, 0) / filtered.length;
+      filtered.reduce((acc, {value}) => acc + value, 0) / filtered.length;
 
   // Find which aqi bands it belongs
-  const index = BANDS.findIndex(({ range }) => range[pollutant][1] >= mean);
+  const index = BANDS.findIndex(({range}) => range[pollutant][1] >= mean);
 
   // Edge Case
   // if it exceeds the set bands
@@ -40,35 +41,36 @@ const calculateSubIndex = (arr = [], pollutant) => {
 
   const [pollutantLo, pollutantHi] = BANDS[index].range[pollutant];
 
-  // this is the basically the line equation where we take the ratio of slopes of
-  // the two lines. two lines representing linear relation between subIndex and concentrations
-  // Linear algebra 101
+  // this is the basically the line equation where we take the ratio of slopes
+  // of the two lines. two lines representing linear relation between subIndex
+  // and concentrations Linear algebra 101
   const aqi =
-    ((aqiHi - aqiLo) / (pollutantHi - pollutantLo)) * (mean - pollutantLo) +
-    aqiLo;
+      ((aqiHi - aqiLo) / (pollutantHi - pollutantLo)) * (mean - pollutantLo) +
+      aqiLo;
   return aqi;
 };
 
 const calculateAQI = (arr) => {
-  //validation
-  if (!Array.isArray(arr) || arr.length === 0) return 0;
+  // validation
+  if (!Array.isArray(arr) || arr.length === 0)
+    return 0;
 
   // Get the data in proper format
   const pollutantArr = arr.reduce((acc, val) => {
     Object.keys(HOUR_DIFF).map((key) => {
       const obj = {
-        datetime: val.datetime,
-        value: val[key],
+        datetime : val.datetime,
+        value : val[key],
       };
-      acc[key] = acc[key] ? [...acc[key], obj] : [obj];
+      acc[key] = acc[key] ? [...acc[key], obj ] : [ obj ];
     });
     return acc;
   }, {});
 
   // Calculate subIndex of each pollutant
-  const subIndexes = Object.keys(pollutantArr).map((key) =>
-    calculateSubIndex(pollutantArr[key], key)
-  );
+  const subIndexes =
+      Object.keys(pollutantArr)
+          .map((key) => calculateSubIndex(pollutantArr[key], key));
 
   // The good stuff
   const nAQI = Math.max(...subIndexes);
--- src/aqi-calculator.test.js
+++ src/aqi-calculator.test.js
@@ -1,34 +1,27 @@
 const calculateAQI = require("./aqi-calculator");
 const testData = require("./test-data");
 
-test("AQI is 0 when null is passed.", () => {
-  expect(calculateAQI(null)).toBe(0);
-});
+test("AQI is 0 when null is passed.",
+     () => { expect(calculateAQI(null)).toBe(0); });
 
-test("AQI is 0 when undefined is passed.", () => {
-  expect(calculateAQI(undefined)).toBe(0);
-});
+test("AQI is 0 when undefined is passed.",
+     () => { expect(calculateAQI(undefined)).toBe(0); });
 
-test("AQI is 0 when string is passed.", () => {
-  expect(calculateAQI("AQI")).toBe(0);
-});
+test("AQI is 0 when string is passed.",
+     () => { expect(calculateAQI("AQI")).toBe(0); });
 
-test("AQI is 0 when empty array is passed.", () => {
-  expect(calculateAQI([])).toBe(0);
-});
+test("AQI is 0 when empty array is passed.",
+     () => { expect(calculateAQI([])).toBe(0); });
 
 test("AQI is 0 when incorrect array is passed.", () => {
-  expect(calculateAQI([123, 324, 567])).toBe(0);
+  expect(calculateAQI([ 123, 324, 567 ])).toBe(0);
 });
 
-test("AQI is 0 when empty object is passed.", () => {
-  expect(calculateAQI({})).toBe(0);
-});
+test("AQI is 0 when empty object is passed.",
+     () => { expect(calculateAQI({})).toBe(0); });
 
-test("AQI is 0 when object is passed.", () => {
-  expect(calculateAQI({ AQI: 323 })).toBe(0);
-});
+test("AQI is 0 when object is passed.",
+     () => { expect(calculateAQI({AQI : 323})).toBe(0); });
 
-test("AQI is greater than 300 when a test data is passed.", () => {
-  expect(calculateAQI(testData)).toBeGreaterThan(300);
-});
+test("AQI is greater than 300 when a test data is passed.",
+     () => { expect(calculateAQI(testData)).toBeGreaterThan(300); });
